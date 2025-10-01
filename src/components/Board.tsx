c9e02a2976f12e98b3ed29ade690246ddea36f43
efraim urel <efraimurelpalodang@gmail.com>
2025-10-01 08:37:29 +0800

    memperbaiki bug logika yang tidak bagus

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -15,7 +15,7 @@ function calculateWinner(squares: any) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
 
-    if (squares[a] && squares[a] === squares[b] && squares[c]) {
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
     }
   }
@@ -23,8 +23,8 @@ function calculateWinner(squares: any) {
 }
 
 export default function Board() {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [xIsNext, setXIsNext] = useState(true);
+  const [squares, setSquares] = useState<Array<string | null>>(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState<boolean>(true);
 
   function handleClick(i: number) {
     if (squares[i] || calculateWinner(squares)) return;
